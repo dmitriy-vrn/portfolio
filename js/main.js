893c344adc11513256fd1d9771e5d1b7e3138be8
Dmitriy Voronin <voronin.dmitriy69@gmail.com>
2020-05-24 20:25:48 +0300

    fonts

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 const navToggle = document.querySelector(".menu-toggle");
 const navTop = document.querySelector(".top-nav");
 const navLinks = document.querySelectorAll(".nav-link");
-const menu = document.querySelector(".top-nav");
 
 function burgerMenu(navToggle) {
   navToggle.classList.toggle("open");
   navTop.classList.toggle("open");
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
-      menu.classList.remove("open");
+      navTop.classList.remove("open");
       navToggle.classList.remove("open");
     });
   });
