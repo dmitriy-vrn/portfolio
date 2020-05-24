1ad104564ee2453b418e9dd84809bd8273fd98d9
Dmitriy Voronin <voronin.dmitriy69@gmail.com>
2020-05-24 19:31:52 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const navToggle = document.querySelector(".menu-toggle");
 const navTop = document.querySelector(".top-nav");
+const navLinks = document.querySelectorAll(".nav-link");
+const menu = document.querySelector(".top-nav");
 
 function burgerMenu(navToggle) {
   navToggle.classList.toggle("open");
   navTop.classList.toggle("open");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("open");
+    });
+  });
 }
